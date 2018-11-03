f6c574284937ee57345dd836c41dbb6be1c23a87
droxt <droxts@gmail.com>
2018-11-03 14:07:33 +0100

    Created project structure

---
+++ src/main/kotlin/com/droxt/officemapper/OfficemapperApplication.kt
@@ -0,0 +1,11 @@
+package com.droxt.officemapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OfficemapperApplication
+
+fun main(args: Array<String>) {
+    runApplication<OfficemapperApplication>(*args)
+}
